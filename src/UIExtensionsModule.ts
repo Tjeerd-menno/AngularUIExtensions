56561cd62f9ee9bf9cfca5481312f9af27eb62a9
Tjeerd-menno Douma <tjeerd.menno@gmail.com>
2015-06-10 15:32:21 +0200

    loadingIndicator name change

--- src/UIExtensionsModule.ts
+++ src/UIExtensionsModule.ts
@@ -6,7 +6,7 @@
     uiExtensionsModule.service("NotificationService", AngularUIExtensions.NotificationService.$inject);
     uiExtensionsModule.controller("NotificationController", AngularUIExtensions.NotificationController.$inject);
     uiExtensionsModule.service("LoadingIndicatorService", AngularUIExtensions.LoadingIndicatorService.$inject);
-    uiExtensionsModule.directive("LoadingIndicatorDirective", AngularUIExtensions.LoadingIndicatorDirective.prototype.Injection())
+    uiExtensionsModule.directive("loadingIndicator", AngularUIExtensions.LoadingIndicatorDirective.prototype.Injection())
     uiExtensionsModule.factory("HttpInterceptorFactory", AngularUIExtensions.HttpInterceptorFactory.$inject);
     
 })() 
